279e171e1dd7047a507457a0dd528b4fa0186811
Melanie Eitel <me@melanieeitel.com>
2015-12-01 21:28:01 -0500

    additional changes to incoming controller

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 # enable user login/logout
 gem 'devise'
-
+gem 'pry-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,27 +2,27 @@ class IncomingController < ApplicationController
 
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
 
   def create
-    
-  # Find the user by using params[:sender]
+    # Find the user by using params[:sender]
     @user = User.find_by(email: params[:sender])
-  # Find the topic by using params[:subject]
-    @topic = Topic.find_by(title: params[:subject])
-  # Assign the url to a variable after retreiving it from params["body-plain"]
-    @url = params["body-plain"]
-  # Check if user is nil, if so, create and save a new user
+    # Check if user is nil, if so, create and save a new user
     if @user.nil?
       @user = User.new(email: params[:sender], password: "temp0rary_passw0rd")
       @user.skip_confirmation!
       @user.save!
     end
-    
+  
+    # Find the topic by using params[:subject]
+    @topic = Topic.find_by(title: params[:subject])    
     # Check if the topic is nil, if so, create and save a new topic
     if @topic.nil?
       @topic = @user.topics.create(title: params[:subject])
     end
-
+  
+    # Assign the url to a variable after retreiving it from params["body-plain"]
+    @url = params["body-plain"]
     @bookmark = @user.bookmarks.create(url: @url, topic_id: @topic.id)
     # Now that you're sure you have a valid user and topic, build and save a new bookmark
     head 200
